696a75595c8388bb4920f640d764091ae8da39c1
m-2squared <176928581+m-2squared@users.noreply.github.com>
2025-09-27 09:02:05 +0200

    chore(Code quality): make prettier ignore line endings
    Because on windows we check out CRLF and want to keep it that way
    
    Signed-off-by: m-2squared <176928581+m-2squared@users.noreply.github.com>

--- config/prettierrc.ts
+++ config/prettierrc.ts
@@ -1,12 +1,17 @@
 import type { Config } from "prettier";
 
 const config: Config = {
+    endOfLine: "auto",
     semi: true,
     trailingComma: "es5",
     singleQuote: false,
     printWidth: 120,
     tabWidth: 4,
     useTabs: false,
+    quoteProps: "as-needed",
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: "always",
 };
 
 export default config;
