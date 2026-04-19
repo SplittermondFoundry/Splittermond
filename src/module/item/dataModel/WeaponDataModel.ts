b325af6ddea4f6758dee1d84a819f64c6b135fe5
m-2squared <176928581+m-2squared@users.noreply.github.com>
2026-04-19 13:00:46 +0200

    Revert "chore(V14): remove migrations from v12"
    This reverts commit e648c411f0a2a5217d1d02d4941348a0aa5e6ae3.

--- src/module/item/dataModel/WeaponDataModel.ts
+++ src/module/item/dataModel/WeaponDataModel.ts
@@ -1,7 +1,14 @@
 import { DataModelSchemaType, fields, SplittermondDataModel } from "../../data/SplittermondDataModel";
 import SplittermondWeaponItem from "../weapon";
 import { attributeName, damage, getDescriptorFields, getPhysicalProperties, validatedBoolean } from "./commonFields";
-import { from13_5_2_migrate_fo_modifiers, from13_8_8_migrateSkillModifiers } from "./migrations";
+import {
+    from0_12_20_migrateDamage,
+    from0_12_20_migrateFeatures,
+    from13_5_2_migrate_fo_modifiers,
+    from13_8_8_migrateSkillModifiers,
+    migrateFrom0_12_13,
+    migrateFrom0_12_20,
+} from "./migrations";
 import { ItemFeaturesModel } from "./propertyModels/ItemFeaturesModel";
 
 function ItemWeaponDataModelSchema() {
@@ -43,6 +50,10 @@ export class WeaponDataModel extends SplittermondDataModel<WeaponDataModelType, 
     static defineSchema = ItemWeaponDataModelSchema;
 
     static migrateData(source: unknown) {
+        source = migrateFrom0_12_13(source);
+        source = migrateFrom0_12_20(source);
+        source = from0_12_20_migrateFeatures(source);
+        source = from0_12_20_migrateDamage(source);
         source = from13_5_2_migrate_fo_modifiers(source);
         source = from13_8_8_migrateSkillModifiers(source);
         return super.migrateData(source);
