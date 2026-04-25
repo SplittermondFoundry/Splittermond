af64a77fa1a3611f1ac10a3dfe10bea639d46de5
m-2squared <176928581+m-2squared@users.noreply.github.com>
2026-04-25 11:34:18 +0200

    feat(Active Effect): proper serialization in data model
    Signed-off-by: m-2squared <176928581+m-2squared@users.noreply.github.com>

--- src/module/activeEffect/dataModel/ModifierDataModel.ts
+++ src/module/activeEffect/dataModel/ModifierDataModel.ts
@@ -1,4 +1,4 @@
-import { DataModelSchemaType, fields } from "../../data/SplittermondDataModel";
+import { DataModelSchemaType, fields, fieldExtensions } from "../../data/SplittermondDataModel";
 import { SplittermondBaseActiveEffect } from "../../data/SplittermondBaseActiveEffect";
 import type { IModifier, ModifierAttributes, ModifierType } from "module/modifiers";
 import type { TooltipFormula } from "module/util/tooltip";
@@ -7,16 +7,21 @@ import {
     asString,
     condense,
     type Expression,
-    evaluate,
     isGreaterZero,
     isLessThanZero,
-    of,
 } from "module/modifiers/expressions/scalar";
+import { serialize, deserialize } from "module/modifiers/expressions/scalar/serialization";
+
+type SerializedExpression = Record<string, unknown> & { type: string };
 
 function ModifierDataModelSchema() {
     return {
         path: new fields.StringField({ required: true, nullable: false }),
-        numericValue: new fields.NumberField({ required: true, nullable: false }),
+        serializedValue: new fieldExtensions.TypedObjectField<SerializedExpression, true, false>({
+            required: true,
+            nullable: false,
+            validate: (v: SerializedExpression) => typeof v === "object" && "type" in v,
+        }),
         selectable: new fields.BooleanField({ required: true, nullable: false, initial: false }),
         attributeName: new fields.StringField({ required: true, nullable: false }),
         attributeType: new fields.StringField({ required: true, nullable: true, initial: null }),
@@ -37,7 +42,7 @@ export class ModifierDataModel extends SplittermondBaseActiveEffect<ModifierData
      * {@link Modifier} constructor signature.
      *
      * @param path Modifier path (used as groupId)
-     * @param value The modifier expression (will be evaluated to a number)
+     * @param value The modifier expression (will be serialized)
      * @param attributes Secondary selection characteristics
      * @param selectable Whether the modifier is selectable as a roll option
      */
@@ -49,7 +54,7 @@ export class ModifierDataModel extends SplittermondBaseActiveEffect<ModifierData
     ): ModifierDataModelType {
         return {
             path,
-            numericValue: evaluate(value),
+            serializedValue: serialize(value),
             attributeName: attributes.name,
             attributeType: attributes.type,
             selectable,
@@ -57,7 +62,7 @@ export class ModifierDataModel extends SplittermondBaseActiveEffect<ModifierData
     }
 
     get value(): Expression {
-        return of(this.numericValue);
+        return deserialize(this.serializedValue);
     }
 
     get isBonus(): boolean {
