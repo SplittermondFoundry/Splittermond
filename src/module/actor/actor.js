69e8e588ffdbe9245a0b830b3879764aa722af62
m-2squared <176928581+m-2squared@users.noreply.github.com>
2026-05-15 12:10:18 +0200

    chore(Active Effect): active effect functions
    Signed-off-by: m-2squared <176928581+m-2squared@users.noreply.github.com>

--- src/module/actor/actor.js
+++ src/module/actor/actor.js
@@ -260,6 +260,10 @@ export default class SplittermondActor extends Actor {
         }
     }
 
+    applyActiveEffects(){
+        super.applyActiveEffects();
+    }
+
     /**@returns VirtualToken[]*/
     getVirtualStatusTokens() {
         return this.items
