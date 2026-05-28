c067ada10376278467dae23ab56248a5c55f7b2c
m-2squared <176928581+m-2squared@users.noreply.github.com>
2026-05-28 15:21:14 +0200

    chore(Active Effect): consolidate integration test
    Signed-off-by: m-2squared <176928581+m-2squared@users.noreply.github.com>

--- src/__tests__/integration/quench.ts
+++ src/__tests__/integration/quench.ts
@@ -15,8 +15,7 @@ import { combatTest } from "./combat.test";
 import { apiConstantsTest } from "./apiConstants.test";
 import { cssVariablesTest } from "./api/cssVariables.test";
 import { hooksTest } from "./hooks.test";
-import { activeEffectDataModelTest } from "./activeEffectDataModel.test";
-import { activeEffectConfigTest } from "./activeEffectConfig.test";
+import { activeEffectTest } from "./activeEffectConfig.test";
 
 declare const Hooks: any;
 declare class Scene extends FoundryDocument {}
@@ -40,8 +39,7 @@ function registerQuenchTests(quench: Quench) {
     quench.registerBatch("splittermond.keybindings", foundryKeybindingsTest);
     quench.registerBatch("splittermond.cssVariables", cssVariablesTest);
     quench.registerBatch("splittermond.hooks", hooksTest);
-    quench.registerBatch("splittermond.activeEffectDataModel", activeEffectDataModelTest);
-    quench.registerBatch("splittermond.activeEffectConfig", activeEffectConfigTest);
+    quench.registerBatch("splittermond.activeEffectConfig", activeEffectTest);
 }
 
 export function init() {
