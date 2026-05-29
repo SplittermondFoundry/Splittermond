1f6bf18b6b391a13c98dac5b64e9f709e27933d5
m-2squared <176928581+m-2squared@users.noreply.github.com>
2026-05-29 14:40:30 +0200

    chore(API): more sophisticated appliccation types
    Signed-off-by: m-2squared <176928581+m-2squared@users.noreply.github.com>

---
+++ src/module/api/foundry-types.d.ts
@@ -0,0 +1,384 @@
+type AnyConstructor = abstract new (...args: never) => unknown;
+type AnyConcreteConstructor = new (...args: never) => unknown;
+
+type Mixin<MixinClass extends AnyConcreteConstructor, BaseClass extends AnyConstructor> = MixinClass & BaseClass;
+
+type DeepPartial<T extends object> = T extends object
+    ? T extends readonly unknown[] | ((...args: never) => unknown) | (abstract new (...args: never) => unknown)
+        ? T
+        : { [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K] }
+    : T;
+
+declare const __AppV2RenderContext: unique symbol;
+declare const __AppV2Configuration: unique symbol;
+declare const __AppV2RenderOptions: unique symbol;
+
+declare const __ApplicationV2Brand: unique symbol;
+
+declare namespace ApplicationV2 {
+    namespace Internal {
+        const __Configuration: typeof __AppV2Configuration;
+        const __RenderOptions: typeof __AppV2RenderOptions;
+        const __RenderContext: typeof __AppV2RenderContext;
+
+        type Constructor = (abstract new (...args: never) => Instance.Any) & {
+            [__ApplicationV2Brand]: never;
+        };
+
+        interface Instance<
+            RenderContext extends object,
+            Configuration extends object,
+            RenderOptions extends object,
+        > {
+            readonly [__RenderContext]: RenderContext;
+            readonly [__Configuration]: Configuration;
+            readonly [__RenderOptions]: RenderOptions;
+        }
+
+        namespace Instance {
+            interface Any extends Instance<any, any, any> {}
+        }
+    }
+
+    type RenderContextOf<Application extends ApplicationV2.Internal.Instance.Any> =
+        Application[typeof ApplicationV2.Internal.__RenderContext];
+}
+
+declare class HandlebarsApplication {
+    constructor(...args: any[]);
+
+    readonly [__AppV2RenderContext]: {};
+    readonly [__AppV2Configuration]: {};
+    readonly [__AppV2RenderOptions]: {};
+
+    static PARTS: Record<string, HandlebarsApplicationMixinNS.HandlebarsTemplatePart>;
+    static TABS: Record<string, foundry.applications.types.ApplicationTabsConfiguration>;
+
+    protected _prepareContext(
+        options: HandlebarsApplicationMixinNS.RenderOptions,
+    ): Promise<foundry.applications.types.ApplicationRenderContext>;
+
+    protected _preparePartContext(
+        partId: string,
+        context: ApplicationV2.RenderContextOf<this>,
+        options: DeepPartial<HandlebarsApplicationMixinNS.RenderOptions>,
+    ): Promise<ApplicationV2.RenderContextOf<this>>;
+
+    protected _onRender(
+        context: foundry.applications.types.ApplicationRenderContext,
+        options: HandlebarsApplicationMixinNS.RenderOptions,
+    ): Promise<void>;
+}
+
+declare function HandlebarsApplicationMixinTyped<BaseClass extends HandlebarsApplicationMixinNS.BaseClass>(
+    BaseApplication: BaseClass,
+): HandlebarsApplicationMixinNS.Mix<BaseClass>;
+
+declare namespace HandlebarsApplicationMixinNS {
+    type BaseClass = ApplicationV2.Internal.Constructor;
+    type Mix<BaseClass extends HandlebarsApplicationMixinNS.BaseClass> = Mixin<typeof HandlebarsApplication, BaseClass>;
+
+    interface RenderOptions {
+        parts: string[];
+    }
+
+    interface HandlebarsTemplatePart {
+        template: string;
+        id?: string | null | undefined;
+        root?: boolean | null | undefined;
+        classes?: string[] | null | undefined;
+        templates?: string[] | null | undefined;
+        scrollable?: string[] | null | undefined;
+        forms?: Record<string, foundry.applications.types.ApplicationFormConfiguration> | null | undefined;
+    }
+}
+
+declare namespace foundry {
+    import ApplicationV2 = foundry.applications.api.ApplicationV2;
+    import ApplicationFormConfiguration = foundry.applications.types.ApplicationFormConfiguration;
+    import ApplicationRenderContext = foundry.applications.types.ApplicationRenderContext;
+    import ApplicationTabsConfiguration = foundry.applications.types.ApplicationTabsConfiguration;
+    import DialogV2 = foundry.applications.api.DialogV2;
+
+    interface DocumentSheetConfiguration {
+        canCreate?: boolean;
+        document: FoundryDocument;
+        editPermission?: number;
+        sheetConfig?: boolean;
+        viewPermission?: number;
+    }
+
+    interface DialogV2Configuration {
+        modal: boolean;
+        buttons: Partial<DialogV2Button>[];
+        content: string;
+        submit: (result: unknown, dialog: DialogV2) => Promise<void>;
+    }
+
+    interface DialogV2Button {
+        action: string;
+        label: string;
+        icon: string;
+        class: string;
+        default: boolean;
+        callback: (
+            event: PointerEvent | SubmitEvent,
+            button: HTMLButtonElement,
+            dialog: foundry.applications.api.DialogV2,
+        ) => Promise<unknown>;
+    }
+
+    interface DialogV2WaitOptions {
+        close?: (event: Event, dialog: DialogV2) => unknown;
+        rejectClose?: boolean;
+        render?: (event: Event, dialog: DialogV2) => unknown;
+    }
+
+    namespace applications {
+        import ApplicationRenderOptions = foundry.applications.types.ApplicationRenderOptions;
+        import ApplicationTab = foundry.applications.types.ApplicationTab;
+        namespace types {
+            type ApplicationClickAction = (event: PointerEvent, target: HTMLElement) => Promise<void>;
+            /**
+             * Not a foundry type
+             */
+            type ApplicationAction =
+                | string
+                | ApplicationClickAction
+                | {
+                      buttons: number[];
+                      handler: ApplicationClickAction;
+                  };
+
+            interface ApplicationTabsConfiguration {
+                initial?: string;
+                labelPrefix?: string;
+                tabs: {
+                    cssClass?: string;
+                    icon?: string;
+                    id: string;
+                    label?: string;
+                    tooltip?: string;
+                }[];
+            }
+
+            interface ApplicationConfiguration {
+                id: string;
+                uniqueId: string;
+                classes: string[];
+                tag: string;
+                window: Partial<ApplicationWindowConfiguration>;
+                actions: Record<string, ApplicationAction>;
+                form: Partial<ApplicationFormConfiguration>;
+                position: Partial<ApplicationPosition>;
+
+                [x: string]: unknown;
+            }
+
+            interface ApplicationWindowConfiguration {
+                frame?: boolean;
+                positioned?: boolean;
+                title?: string;
+                icon?: string | false;
+                controls: ApplicationHeaderControlsEntry[];
+                minimizable?: boolean;
+                resizable?: boolean;
+                contentTag?: string;
+                contentClasses?: string[];
+            }
+
+            interface ApplicationFormConfiguration {
+                handler: (event: Event, form: unknown, formData: unknown) => void;
+                submitOnChange: boolean;
+                closeOnSubmit: boolean;
+            }
+
+            interface ApplicationHeaderControlsEntry {
+                icon: string;
+                label: string;
+                action: string;
+                visible: boolean;
+                ownership: string | number;
+            }
+
+            interface ApplicationPosition {
+                top: number;
+                left: number;
+                width: number | "auto";
+                height: number | "auto";
+                scale: number;
+                zIndex: number;
+            }
+
+            interface ApplicationRenderOptions {
+                force?: boolean;
+                isFirstRender?: boolean;
+                parts?: string[];
+                position?: ApplicationPosition;
+                tab?: string | Record<string, string>;
+                window?: ApplicationWindowRenderOptions;
+            }
+
+            interface ApplicationWindowRenderOptions {
+                controls: boolean;
+                icon: string | false;
+                title: string;
+            }
+
+            interface ApplicationRenderContext {
+                tabs?: Record<string, ApplicationTab>;
+
+                [x: string]: unknown;
+            }
+
+            interface ApplicationTab {
+                active: boolean;
+                cssClass: string;
+                group: string;
+                icon?: string;
+                id: string;
+                label?: string;
+                tooltip?: string;
+            }
+        }
+
+        namespace api {
+            import ApplicationConfiguration = foundry.applications.types.ApplicationConfiguration;
+            import ApplicationRenderOptions = foundry.applications.types.ApplicationRenderOptions;
+            import ApplicationRenderContext = foundry.applications.types.ApplicationRenderContext;
+            import FormDataExtended = foundry.applications.ux.FormDataExtended;
+            import ApplicationTab = foundry.applications.types.ApplicationTab;
+
+            /**
+             * Type declarations for applications. incomplete, copied at V13
+             * @see https://foundryvtt.com/api/classes/foundry.applications.api.DialogV2.html
+             */
+            export class DialogV2 extends ApplicationV2<ApplicationConfiguration & foundry.DialogV2Configuration> {
+                constructor(config: Partial<foundry.applications.types.ApplicationConfiguration &
+            foundry.DialogV2Configuration>);
+
+                static confirm(config: { content: string; rejectClose: boolean; modal: true }): Promise<boolean>;
+
+                static prompt(
+                    config: Partial<ApplicationConfiguration & DialogV2Configuration & DialogV2WaitOptions>,
+                ): Promise<unknown>;
+
+                render(options?: DialogV2RenderOptions): Promise<this>;
+
+                get form(): null | HTMLFormElement;
+            }
+
+            /**
+             * Type declarations for applications. incomplete, copied at V13
+             * @see https://foundryvtt.com/api/classes/foundry.applications.api.ApplicationV2.html
+             */
+            export class ApplicationV2<
+                CONFIGURATION extends ApplicationConfiguration = ApplicationConfiguration,
+                RENDER_OPTIONS = ApplicationRenderOptions,
+            > {
+                static [__ApplicationV2Brand]: never;
+
+                [__AppV2RenderContext]: ApplicationRenderContext;
+                [__AppV2Configuration]: CONFIGURATION;
+                [__AppV2RenderOptions]: RENDER_OPTIONS;
+
+                constructor(options?: Partial<CONFIGURATION>);
+
+                options: Readonly<CONFIGURATION>;
+
+                get classList(): DOMTokenList;
+
+                get element(): HTMLElement;
+
+                get form(): HTMLFormElement | null;
+
+                get rendered(): boolean;
+
+                protected _prepareContext(options: RENDER_OPTIONS): Promise<ApplicationRenderContext>;
+
+                render(options?: boolean | ApplicationRenderOptions): Promise<this>;
+
+                submit(submitOptions?: object): Promise<any>;
+
+                addEventListener(type: string, listener: (event: Event) => void): void;
+
+                close(): void;
+                _preClose(options: ApplicationRenderOptions): Promise<void>;
+
+                protected _onRender(context: ApplicationRenderContext, options: RENDER_OPTIONS): Promise<void>;
+
+                protected _onSubmitForm(
+                    formConfig: ApplicationFormConfiguration,
+                    event: Event | SubmitEvent,
+                ): Promise<void>;
+
+                protected _prepareTabs(group: string): Record<string, ApplicationTab>;
+            }
+
+            export class DocumentSheetV2 extends foundry.applications.api.ApplicationV2 {
+                constructor(options: any, ...args: any[]);
+                options: ApplicationConfiguration & DocumentSheetConfiguration;
+                get document(): FoundryDocument;
+                _prepareSubmitData(
+                    event: SubmitEvent,
+                    form: HTMLFormElement,
+                    formData: FormDataExtended,
+                    updateData?: object,
+                ): object;
+            }
+
+            export const HandlebarsApplicationMixin: typeof HandlebarsApplicationMixinTyped;
+        }
+
+        namespace ux {
+            class DragDrop {
+                constructor(config: Partial<DragDropConfiguration>);
+                permissions: Record<"dragstart" | "drop", (selector: string) => boolean>;
+                bind(html: HTMLElement): this;
+            }
+
+            class FormDataExtended {
+                //This class is a stub. Doc found https://foundryvtt.com/api/classes/foundry.applications.ux.FormDataExtended.html
+            }
+        }
+
+        namespace sheets {
+            import DocumentSheetV2 = foundry.applications.api.DocumentSheetV2;
+            import HandlebarsApplicationMixin = foundry.applications.api.HandlebarsApplicationMixin;
+
+            class ItemSheetV2 extends DocumentSheetV2 {
+                get actor(): Actor | null;
+                get item(): Item;
+            }
+
+            class ActorSheetV2 extends DocumentSheetV2 {
+                get actor(): Actor;
+
+                _onDropDocument<T extends FoundryDocument>(
+                    event: DragEvent,
+                    document: FoundryDocument,
+                ): Promise<null | T>;
+
+                _onDragStart(event: DragEvent): Promise<void>;
+
+                _canDragStart(selector: string): boolean;
+                _onDrop(event: DragEvent): Promise<void>;
+            }
+
+            class ActiveEffectConfig extends HandlebarsApplicationMixin(DocumentSheetV2) {
+                _preparePartContext(partId: string, context: object, options: object): Promise<ApplicationRenderContext>;
+                _processFormData(event: Event, form: HTMLFormElement, formData: unknown): object;
+            }
+        }
+    }
+
+    interface DragDropConfiguration {
+        callbacks?: Record<
+            "dragstart" | "drop" | "dragover" | "dragenter" | "dragleave" | "dragend",
+            (event: DragEvent) => void
+        >;
+        dragSelector?: null | string;
+        dropSelector?: null | string;
+        permissions?: Record<"dragstart" | "drop", (selector: string) => boolean>;
+    }
+}
