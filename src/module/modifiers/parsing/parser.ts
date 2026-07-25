a5bd881a47fd726c49d87536be4297a0aa669824
m-2squared <176928581+m-2squared@users.noreply.github.com>
2026-07-25 18:00:28 +0200

    feature(Modifier): allow value-less modifiers
    Signed-off-by: m-2squared <176928581+m-2squared@users.noreply.github.com>

--- src/module/modifiers/parsing/parser.ts
+++ src/module/modifiers/parsing/parser.ts
@@ -72,7 +72,11 @@ function parseModifier(modifier: string): SingleParseResult {
     const value = valueMatch ? parseValue(valueMatch[0]) : null;
     if (isSet(value) && isSet(attributeParseResult.value)) {
         return foundryApi.format("splittermond.modifiers.parseMessages.duplicateValue", { modifier });
-    } else if (!isSet(value) && !isSet(attributeParseResult.value)) {
+    } else if (
+        !isSet(value) &&
+        !isSet(attributeParseResult.value) &&
+        !hasNonValueAttribute(parseResult.attributes)
+    ) {
         return foundryApi.format("splittermond.modifiers.parseMessages.noValue", { modifier });
     } else if (isSet(value)) {
         parseResult.attributes.value = value;
@@ -82,6 +86,9 @@ function parseModifier(modifier: string): SingleParseResult {
 
 function isSet(value: unknown) {
     return value !== null && value !== undefined;
+}
+function hasNonValueAttribute(attributes: Record<string, unknown> ): boolean {
+    return Object.keys(attributes).filter((k) => k !== "value").length > 0
 }
 
 function findAttributes(modifier: string): string[] {
